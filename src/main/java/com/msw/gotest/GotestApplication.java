c1e6c9ec2e7300d6fe0f3b39b4a52e0785c541dc
unknown <masw@yintong.com.cn>
2018-07-12 17:57:41 +0800

    create

---
+++ src/main/java/com/msw/gotest/GotestApplication.java
@@ -0,0 +1,12 @@
+package com.msw.gotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GotestApplication.class, args);
+    }
+}
---
+++ src/main/java/com/msw/gotest/mapper/MybatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.msw.gotest.mapper;
+
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.msw.gotest.mapper")
+public class MybatisPlusConfig {
+    /*
+     * 分页插件，自动识别数据库类型
+     * 多租户，请参考官网【插件扩展】
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+}
